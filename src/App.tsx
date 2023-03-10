cf060ee120b0dc9da9b5fbd5ecc200ccd7d8c62d
Roman Coropat <roman.koropat@gmail.com>
2023-03-10 19:09:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useMemo, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { NewNote } from './components/NewNote'
+import { useLocalStorage } from './hooks/useLocalStorage'
+import {v4 as uuidV4} from 'uuid'
+import { NoteList } from './components/NoteList'
+import { NoteLayout } from './components/NoteLayout'
+import { Note } from './components/Note'
+import { EditNote } from './components/EditNote'
+
+
+export type Tag = {
+  id: string
+  label: string
+}
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Note = {
+  id: string
+} & NoteData
+
+function App() {
+
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+  const [bgTheme, setBgTheme] = useState('dark')
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes, {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}]
+    })
+  }
+
+  function onUpdateNote(id: string, { tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id) {
+          return {...note, ...data, tagIds: tags.map(tag => tag.id)}
+        } else {
+          return note
+        }
+      })
+    })
+  }
+
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
+  function addTag(tag: Tag) {
+    setTags(prev => [...prev, tag])
+  }
+
+  function updateTag(id: string, label: string) {
+    setTags(prevTags => {
+      return prevTags.map(tag => {
+        if (tag.id === id) {
+          return {...tag, label}
+        } else {
+          return tag
+        }
+      })
+    })
+  }
+
+  function deleteTag(id: string) {
+    setTags(prevTags => {
+      return prevTags.filter(tag => tag.id !== id)
+    })
+  }
+
+  return (
+    <div className={bgTheme}>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<NoteList notes={notesWithTags} availableTags={tags} onUpdateTag={updateTag} onDeleteTag={deleteTag}/>}></Route>
+        <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags}/>} />
+        <Route path='*' element={<Navigate to='/' />} />
+        <Route path='/:id' element={<NoteLayout notes={notesWithTags}/>}>
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route path='edit' element={<EditNote onSubmit={onUpdateNote} onAddTag={addTag} availableTags={tags} />}/>
+        </Route>
+      </Routes>
+    </Container>
+    </div>
+  )
+}
+
+export default App
